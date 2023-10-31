8845c971a722ece662954ab67b305423e92b2ae7
ggmaddr <ggiangta@gmail.com>
2023-10-31 05:09:57 -0700

    36. Valid Sudoku

--- leet.py
+++ leet.py
@@ -1,20 +1,26 @@
 from collections import defaultdict 
 #36. Valid Sudoku
 def isValidSudoku(board: list[list[str]]) -> bool:
-    #create hashset of cols, rows and sqr
-    cols = defaultdict(set)
+    #Idea: check if each small square belongs to coordinated-associated rows, cols, and sqr 
+    #Implement: create hashset (dict of sets) as cols, rows and sqr
+    #each row, col, sqr is a set
+    #Form of each dict(set): e.g: rows = {0: {set}, 1:{set},...}
+    cols = defaultdict(set) 
     rows = defaultdict(set)
     sqr = defaultdict(set)
 
-def def_value(): 
-    return "Present"
-      
-# Defining the dict 
-d = defaultdict(def_value) 
-d["a"] = 1
-d["b"] = 2
-  
-print(d["a"]) 
-print(d["b"]) 
-print(d["c"]) 
-    
+    for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
+                    continue
+                if (
+                    board[r][c] in rows[r]
+                    or board[r][c] in cols[c]
+                    or board[r][c] in sqr[(r // 3, c // 3)]
+                ):
+                    return False
+                cols[c].add(board[r][c])
+                rows[r].add(board[r][c])
+                sqr[(r // 3, c // 3)].add(board[r][c])
+
+    return True
