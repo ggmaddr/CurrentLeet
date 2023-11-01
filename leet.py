5a409efc6a713ae909cdfc62c68d0109841e35c7
ggmaddr <ggiangta@gmail.com>
2023-11-01 05:09:53 -0700

    238. Product of Array Except Self. Only pos var

--- leet.py
+++ leet.py
@@ -1,22 +1,22 @@
 def productExceptSelf(nums: list[int]) -> list[int]:
-    #Create 2 arrays: prefix product and postfix product.
-    #Output[i] = prefix[i-1] * postfix [i+1]
-    #Time: O(n), Space: O(3n)
+    #Create an preArr stores prefix product up to + (exclude) nums[i]
+    #Loop from the end of preArr: at each preArr[i], multiply by current postfix product var
+
+    #Time: O(n), Space: O(n)
     l = len(nums)
-    pre, pos= [nums[0]]*l, [nums[l-1]]*l
-    j = l - 2
+    out = [1] * l
+    
     for i in range(1, l):
-        pre[i] = pre[i-1] * nums[i]
-        pos[j] = pos[j+1] * nums[j]
-        j-=1
+         #Create an preArr stores prefix product up to nums[i] + (exclude) nums[i]
+        out[i] = out[i-1] * nums[i-1]
+    print(out)
     #init first and last in output
-    out = [pos[1]]*l
-    out[l-1] = pre[l-2]
+    pos = nums[l-1]
+    for i in range(l-2, -1, -1):
+        out[i] = out[i] * pos  #upto prefix * upto postfix 
+        pos *= nums[i]
     
-    for i in range(1,l-1):
-        out[i] = pre[i-1]*pos[i+1]
-    
-    print(pre, pos, out)
+    print(out)
     return out
 
 nums = [1,2,3,4]
