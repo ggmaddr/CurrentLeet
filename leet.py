56759399cb8fa659914c5e389cd76b8aad223b5d
ggmaddr <ggiangta@gmail.com>
2023-11-02 06:59:38 -0700

    167. Two Sum II - Input Array Is Sorted

--- leet.py
+++ leet.py
@@ -1,21 +1,19 @@
-
-def longestConsecutive(nums: list[int]) -> int:
-    #Sort and count the highest
-    nums.sort()
-    if len(nums) == 0:
-        return 0
-    max = 1
-    temp = 1
-    for i in range(1, len(nums)):
-        if nums[i] == nums[i-1]+1: 
-            temp+=1
-        elif nums[i] == nums[i-1]:
-            continue
+def twoSum(numbers: list[int], target: int) -> list[int]:
+    #2 pointers: at start and at end. 
+    # When sum(nums[i]+nums[j])> target -> decrement end p (decrease larger num in the sum)
+    #sum(nums[i]+nums[j]) < target -> increase start p 
+    #Time: O(n), Space: O(1)
+    
+    pre, pos = 0, len(numbers)-1
+    while pre < pos:
+        sum = numbers[pre]+numbers[pos]
+        if sum < target:
+            pre += 1
+        elif sum > target:
+            pos -= 1
         else:
-            temp = 1 #reset
-        if temp > max:
-            max = temp
-    print(max)
-    return max
-nums = [100,4,200,1,3,2]
-longestConsecutive(nums)
+            return [pre+1, pos+1]
+    return []
+numbers = [2,7,11,15]
+target = 9
+print(twoSum(numbers, target))
