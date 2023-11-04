8c9603be2f79b5a38140774786a07cf011664030
ggmaddr <ggiangta@gmail.com>
2023-11-04 04:57:40 -0700

    next

--- leet.py
+++ leet.py
@@ -1,33 +1,20 @@
-def threeSum(nums: list[int]) -> list[list[int]]:
-    print("TEST RESULT: -------------------------------------")
-    nums.sort()
-    res = []
-    print(nums)
-    i, k, j = 0, 1, len(nums)-1
-    while k < j:
-        if nums[i]+nums[k]+nums[j] == 0:
-            return [[nums[i],nums[k],nums[j]]]     
-             
-        while nums[i] + nums[j] + nums[j] <= 0:
-            i+=1
-            if i == len(nums):
-                return res
-        
-        k = i+1
-        print(i, k, j)
-        if nums[i]+nums[j]+nums[k] == 0:
-            res.append([nums[i],nums[k],nums[j]])
-            if
-            j-=1
-            i+=1
-        else:
-            if nums[i]+nums[j]+nums[k] < 0:
-                i+=1
-            else:
-                j-=1
-    return res
-nums = [-1,0,1,2,-1,-4]
+def isValid(s: str) -> bool:
+    brack = {'{':'}', '[':']', '(':')'}
+    stack = []
+    for i in range(len(s)):
+        if s[i] in brack.keys():
+            stack.append(s[i])
+        elif len(stack) != 0:
+            if s[i] != brack[stack[-1]]: #peek
+                return False
+            stack.pop()
+        # len(stack) == 0 + still have non-open char -> illegal close char
+        elif len(stack) == 0: 
+            return False
+    if len(stack) != 0:
+        return False
+    return True
 
-nums1= [-2,0,1,1,2]
-display(threeSum(nums1))
-display(threeSum(nums2))
+
+s = "{[]}"
+print(isValid(s))
