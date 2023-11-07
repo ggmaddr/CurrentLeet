ff67fde6ccbd69c792caec960131257d4573c534
ggmaddr <ggiangta@gmail.com>
2023-11-07 06:34:49 -0800

    stack

--- leet.py
+++ leet.py
@@ -1,20 +1,26 @@
-def isValid(s: str) -> bool:
-    brack = {'{':'}', '[':']', '(':')'}
-    stack = []
-    for i in range(len(s)):
-        if s[i] in brack.keys():
-            stack.append(s[i])
-        elif len(stack) != 0:
-            if s[i] != brack[stack[-1]]: #peek
-                return False
-            stack.pop()
-        # len(stack) == 0 + still have non-open char -> illegal close char
-        elif len(stack) == 0: 
-            return False
-    if len(stack) != 0:
-        return False
-    return True
 
+def generateParenthesis(n: int) -> list[str]:
+    out = []
+    comb = [] #each combination
+    #recursion with # of o and c parentheses from 0 to n
+    
+    def backtrack(ocount, ccount):
+        #base case
+        if ocount == n and ccount == n:
+            out.append("".join(comb))
+        
+        if ocount < n:
+            comb.append('(')
+            backtrack(ocount+1, ccount)
+            comb.pop()
+        if ccount < ocount: 
+            comb.append(')')
+            backtrack(ocount, ccount+1)
+            comb.pop()
 
-s = "{[]}"
-print(isValid(s))
+    
+    backtrack(0,0)
+    return out
+
+n = 3
+print(generateParenthesis(n))
