782254a9b7933877b2b4a38b4bdee32ddcfecda9
ggmaddr <ggiangta@gmail.com>
2023-11-18 02:35:40 -0800

    dup

--- leet.py
+++ leet.py
@@ -39,45 +39,25 @@ def create_list(values):
         current = current.next
     return head
 
-import math
-def reorderList(head: Optional[ListNode]) -> None:
-    """
-    Do not return anything, modify head in-place instead.
-    """
-    # find middle
-    s, f = head, head
-    while f.next and f.next.next:
-        s = s.next
-        f = f.next.next
+def removeNthFromEnd(head: Optional[ListNode], n: int) -> Optional[ListNode]:
     
-    #split
-    se = s.next #saved second half
-    s.next = None #cut tail at mid
-    #mid 's' still belongs to head -> cut tail of head
+    dummy = ListNode()
+    #Dummy to remove head.
+    dummy.next = head 
+    l, r = dummy, head
+    while n > 0:
+       r = r.next
+       n -=1
+    #push r to null
+    while r: 
+        r = r.next
+        l = l.next
+    l.next = l.next.next
     
-    #Reverse
-    prev = None
-    while se:
-        tm = se.next
-        se.next = prev
-        prev = se
-        se = tm
-    
-    se = prev #prev is reversed list
-    
-    #Merge 2 halves
-    
-    fi = head #first half
-    
-    while fi and se:
-        fnext, snext = fi.next, se.next
-        fi.next = se
-        se.next = fnext
-        fi, se = fnext,snext
-    return head #for testing
-    
-head = [1,2,3,4,5,6,7,8,9]
+    return dummy.next
+
+head = [1,2,3]
 
 head1 = create_list(head)
-
-print(reorderList(head1))
+print(removeNthFromEnd(head1,1))
+    
