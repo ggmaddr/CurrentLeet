299c545a40393ee203e24dbfd7cafc9b6507f72c
ggmaddr <ggiangta@gmail.com>
2023-11-21 06:14:42 -0800

    a

--- leet.py
+++ leet.py
@@ -1,73 +1,26 @@
-from typing import Optional
+def maxRepeating(sequence: str, word: str) -> int:
+    i,j = 0, len(word)
+    ccount, mxcount = 0,0 #currentcount, maxcount
 
-# Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-    def __str__(self):
-        s = "("+ str(self.val) + ") => "
-        tm = self.next
-        while tm:
-            s += str(tm.val) + " -> "
-            tm = tm.next
-        return s
-    def __repr__(self):
-        s = "("+ str(self.val) + ") => "
-        tm = self.next
-        while tm:
-            s += str(tm.val) + " -> "
-            tm = tm.next
-        return s
-            
-            
-
-def print_list(head: Optional[ListNode]) -> None:
-    while head:
-        print(head.val, end=" -> ")
-        head = head.next
-    print("None")
-
-# Helper function to create a linked list from a list of values
-def create_list(values):
-    if not values:
-        return None
-    head = ListNode(values[0])
-    current = head
-    for val in values[1:]:
-        current.next = ListNode(val)
-        current = current.next
-    return head
-
-def addTwoNumbers(l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        res = ListNode()
-        temp = res
-        carr = 0 #carry
-        
-        #finsih loop while both l1 and l2 end
-        while l1 or l2:
-            
-            if l1 and l2:
-                ssum = l1.val + l2.val + carr
-            elif l1:
-                ssum = l1.val + carr if l1 else l2.val + carr
-        
-            temp.val = ssum%10
-            carr = ssum//10
-
-            l1 = l1.next if l1 else None
-            l2 = l2.next if l1 else None
-            if l1 or l2:
-                temp.next = ListNode()
-                temp = temp.next
+    while j<=len(sequence):
+            temp = sequence[i:j]
+            if temp == word:
+                ccount +=1
+                i += len(word) #on going success
+                j += len(word)
+            elif temp !=word:
+                #reset ccount if ccount > 0
+                if ccount > 0:
+                    mxcount = max(mxcount, ccount)
+                    ccount = 0
+                i +=1
+                j +=1
+    mxcount = max(mxcount, ccount)
+    return mxcount
                 
-        if carr == 1:
-            temp.next = ListNode()
-            temp = temp.next
-        
-        return res
-l1 = create_list([9,9,9,9,9,9,9])
-l2 = create_list([9,9,9,9])
-addTwoNumbers(l1, l2)
-
     
+# Example usage:
+short_str = "aaaba"
+long_str = "aaabaaaabaaabaaaabaaaabaaaabaaaaba"
+result = maxRepeating(long_str, short_str)
+print(result)
