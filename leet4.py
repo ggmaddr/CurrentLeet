a78a92c5fa20ab4eba33510f90d94ca4b206eb53
Grady Ta <giang.ta@sjsu.edu>
2024-10-20 02:36:50 -0700

    20/10

--- leet4.py
+++ leet4.py
@@ -1,67 +1,17 @@
-from typing import Optional
-
-# Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-    def __repr__(self):
-        s = "("+ str(self.val) + ") => "
-        tm = self.next
-        while tm:
-            s += str(tm.val) + " -> "
-            tm = tm.next
-        return s
-
-def print_list(head: Optional[ListNode]) -> None:
-    while head:
-        print(head.val, end=" -> ")
-        head = head.next
-    print("None")
-
-# Helper function to create a linked list from a list of values
-def create_list(values):
-    if not values:
-        return None
-    head = ListNode(values[0])
-    current = head
-    for val in values[1:]:
-        current.next = ListNode(val)
-        current = current.next
-    return head
-import math
-import copy
-def reorderList(head: Optional[ListNode]) -> None:
-    """
-    Do not return anything, modify head in-place instead.
-    """
-    if head.next == None or head.next.next == None:
-        return head
-        
-    cur = head
-    mlist = list()
-    while cur:
-        mlist.append(cur)
-        cur = cur.next
+def dailyTemperatures(temperatures: list[int]) -> list[int]:
     
-    r = len(mlist) - 1
-    stop =len(mlist) //2
+    #Pattern reg: find nearest correlation -> stack.pop get the most recent val within the correlation.
 
-    for l in range (stop):
-        # todo: examine 3-4-5 case.   
-        mlist[l].next = mlist[r]
-        mlist[r].next = mlist[l+1]
-        r-=1
-    if len(mlist) % 2 == 0:
-        mlist[r+1].next = None
-    else:
-        mlist[r].next = None
-    return head
+    st = [] #arrays of tuple (temp:index)
+    out = [0]*len(temperatures)
+    #Add temps to stack maintaining decreasingly monotonic, 
+    for i,t in enumerate(temperatures):
+        #only (keep) pop when found the nearest warm (increase t)
+        while st and t > st[-1][0]: #st[-1][0] = top stack temp
+            stTemp,stIndex = st.pop()
+            out[stIndex] = i - stIndex            
+        st.append((t,i)) #i, t
 
+    return out
 
-head = [1,2,3,4,5]
-
-head1 = create_list(head)
-print(head1)
-
-print(reorderList(head1))
+print(dailyTemperatures([73,74,75,71,69,72,76,73]))
