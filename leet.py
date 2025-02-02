69252d7c389732faae856279f60295f9425e9d85
Grady Ta <giang.ta@sjsu.edu>
2025-02-02 02:23:30 -0800

    latest

--- leet.py
+++ leet.py
@@ -1,62 +1,103 @@
-from collections import defaultdict, deque
+# def longest_palindromic_substring(s):
+#     # preprocess the string to insert '#' between characters
+#     # and '^' at the start and '$' at the end to simplify boundaries.
+#     t = "^#" + "#".join(s) + "#$"
+#     n = len(t)
+#     p = [0] * n  # Array to store the length of the palindrome radius
+#     C = 0  # Center of the current palindrome
+#     R = 0  # Right boundary of the current palindrome
+#     mmax = 0
+#     mword = ""
+#     for i in range(1, n - 1):
+#         # Mirror of the current position `i` with respect to center `C`
+#         mirror = 2 * C - i
 
-def getMinSumNodeValues(service_nodes, service_from, service_to, k, currentValues):
-    # Step 1: Construct the graph
-    graph = defaultdict(list)
-    for i in range(service_nodes - 1):
-        graph[service_from[i]].append(service_to[i])
-        graph[service_to[i]].append(service_from[i])
+#         # Use previously computed palindrome length if within boundary
+#         #limit p[i] to R - i, we ensure that: palindrome at i within boundary. Further expansion will only happen if the characters beyond R match symmetrically around i, which will be handled by the while loop that follows.
+#         if i < R:
+#             p[i] = min(R - i, p[mirror]) #If i too close to bound R, pal might get "cut off" by R
 
-    # Step 2: Initialize threads array
-    threads = [-1] * service_nodes
-
-    # Step 3: Update known configurations
-    for node, value in currentValues:
-        threads[node - 1] = value
-
-    # Step 4: BFS traversal to assign ranges
-    visited = set()
-    queue = deque(currentValues)
-    while queue:
-        node, value = queue.popleft()
-        if node in visited:
-            print("GET VISITED")
-            continue
-        visited.add(node)
-        print(node, value)
-        for neighbor in graph[node]:
-            if isinstance(threads[neighbor-1], int) and threads[neighbor-1] > 0 :
-                continue
+#         # Expand around center `i`
+#         a,b = t[i + p[i] + 1], t[i - p[i] - 1]
+#         while t[i + p[i] + 1] == t[i - p[i] - 1]:
+#             p[i] += 1
+       
             
-            #if not assigned and next to nint
-            if threads[neighbor-1] == -1 and isinstance(value, int):
-                threads[neighbor-1] = [value-1, value+1]
-
-            #if neighbor not assigned and next to a range
-            elif threads[neighbor-1] == -1 and isinstance(value, list):
-                threads[neighbor-1] = [value[0]-1, value[1]+1]
-            #if neighbor is an assigned range
-            else:
-                
-                
-                
-                newmin = min(value[1]+1, threads[neighbor-1][0])
-                newmax = max(value[0]-1,threads[neighbor-1][1])
-                
-                threads[neighbor-1] = [newmin, newmax]
-            queue.append((neighbor, threads[neighbor - 1]))
-            print(threads)
-    
-    
+#         # Update center and right boundary
+#         if i + p[i] > R:
+#             C, R = i, i + p[i]
             
-    
-    # Step 5: Return result
-    return threads
+#         if p[i] > mmax:
+#             mmax = p[i]
+#             mword = t[p[i]-(R-p[i]:p[i])]+t[p[i]:R]
 
-# Example usage:
-service_nodes = 6
-service_from = [1, 2, 3, 4,6]
-service_to = [2, 3, 4, 5,3]
-k = 3
-currentValues = [[1, 3], [5, 3], [6, 6]]
-print(getMinSumNodeValues(service_nodes, service_from, service_to, k, currentValues))
+#     # Find the longest palindrome considering lexicographical order
+#     max_len = 0
+#     start_index = 0
+#     for i in range(1, n - 1):
+#         if p[i] > max_len:
+#             max_len = p[i]
+#             start_index = (i - max_len) // 2
+#         elif p[i] == max_len:  # If lengths are equal, check lexicographical order
+#             temp = (i - max_len) // 2
+#             if s[temp:temp + max_len] < s[start_index:start_index + max_len]:
+#                 start_index = temp
+
+#     return s[start_index:start_index + max_len]
+
+# # s = "abacdfgdcabba"
+# s = "baabaab"
+
+# print(longest_palindromic_substring(s))  # Output: "abba"
+
+
+
+def leftmostBuildingQueries(heights, queries):
+    mono_stack = []
+    result = [-1 for _ in range(len(queries))]
+    new_queries = [[] for _ in range(len(heights))]
+    for i in range(len(queries)):
+        a = queries[i][0]
+        b = queries[i][1]
+        if a > b:
+            a, b = b, a
+        if heights[b] > heights[a] or a == b:
+            result[i] = b
+        else:
+            new_queries[b].append((heights[a], i))
+    print(new_queries)
+    for i in range(len(heights) - 1, -1, -1):
+        mono_stack_size = len(mono_stack)
+        for a, b in new_queries[i]:
+            print("@@AT height", i, "query", b)
+            position = search(a, mono_stack)
+            if position >= mono_stack_size:
+                print("&&&pos < len happens with")
+            if position >= 0:
+                result[b] = mono_stack[position][1]
+            
+        while mono_stack and mono_stack[-1][0] <= heights[i]:
+            print("idx pop top", i, mono_stack[-1][0])
+            mono_stack.pop()
+        mono_stack.append((heights[i], i))
+    return result
+
+def search(height, mono_stack):
+    left = 0
+    right = len(mono_stack) - 1
+    ans = -1
+    while left <= right:
+        
+        mid = (left + right) // 2
+        print("mid idx-val - height is ", mid, mono_stack[mid][0], height )
+        if mono_stack[mid][0] > height:
+            ans = max(ans, mid)
+            left = mid + 1
+        else:
+            right = mid - 1
+    return ans
+heights = [5,3,8,2,3,5,2,4,6,5,5,7]
+queries = [(0,3), (5,6), (8,10), (0,6), (7,2), (6,1), (1,5), (0,7), (3,5)]
+print(leftmostBuildingQueries( heights, queries))
+    
+    
